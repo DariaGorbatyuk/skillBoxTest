0057ca3d6faad37084abb2c60b844dc31233c464
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-05-15 12:06:54 +0300

    Добавляет валидацию форму для email

--- build/js/script.js
+++ build/js/script.js
@@ -3,15 +3,17 @@
   const $form = document.querySelector('.sub-form');
   const $name = $form.querySelector('#name');
   const $tel = $form.querySelector('#tel');
+  const $email = $form.querySelector('#email');
   const MIN_LENGTH_TEL = 11;
   const MIN_LENGTH_NAME = 2;
 
   $name.addEventListener('input', onNameInput);
   $tel.addEventListener('input', onTelInput);
+  $email.addEventListener('change', onEmailChange);
 
 
   function onNameInput(evt) {
-    const regexp = /[а-яА-яA-Za-z]/;
+    const regexp = /[а-яА-яA-Za-z\s]/;
     evt.currentTarget.addEventListener('keypress', function (e) {
       if (!regexp.test(e.key)) {
         e.preventDefault();
@@ -35,6 +37,17 @@
       $tel.parentElement.classList.add('sub-form__box--short')
     } else {
       $tel.parentElement.classList.remove('sub-form__box--short')
+    }
+  }
+
+  function onEmailChange(evt) {
+    const regexp = /.+@.+\..+/i;
+    if (!regexp.test(evt.currentTarget.value)) {
+      $email.parentElement.classList.add('sub-form__box--wrong');
+      $email.setCustomValidity('Email должен содержать "@" и "."');
+    } else {
+      $email.parentElement.classList.remove('sub-form__box--wrong');
+      $email.setCustomValidity('');
     }
   }
 })()
